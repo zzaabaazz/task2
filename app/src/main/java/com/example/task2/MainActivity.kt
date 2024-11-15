17847359e79f5bca3f757a644d445ea734f0ec15
zzaabaazz <zzaabaazz@gmail.com>
2024-11-15 14:09:49 +0300

    0.3 New Calculator activity(not working)

--- app/src/main/java/com/example/task2/MainActivity.kt
+++ app/src/main/java/com/example/task2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.task2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -15,11 +16,17 @@ class MainActivity : AppCompatActivity() {
 // получение EditText по ID
         val mHelloTextView = findViewById<TextView>(R.id.tVName)
         val button = findViewById<Button>(R.id.btnHello)
+
         button.setOnClickListener {
             if (mNameEditText.length() == 0) { //проверка, что имя введено
                 mHelloTextView.setText("Здравствуй, NoName!"); } else {
                 mHelloTextView.setText("Привет, " + mNameEditText.text);
             }
+        }
+        val towardsCalculator = findViewById<Button>(R.id.toCalc)
+        towardsCalculator.setOnClickListener {
+            val intent = Intent(this, Calculator::class.java)
+            startActivity(intent)
         }
     }
 }
